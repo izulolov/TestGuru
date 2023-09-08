f8ab7202b55df1b3ecc1ced8c0c7e44cb20a5fe3
izulolov <izulolov@mail.ru>
2023-09-08 17:10:11 +0300

    testcategories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,87 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create!([{name: 'Ikbol', score: 0},
+          {name: 'Artem', score: 0},
+          {name: 'Dmitry', score: 0},
+          {name: 'Aleksey', score: 0},
+          {name: 'Dmitry', score: 0},
+          {name: 'Anton', score: 0},
+          {name: 'Petr', score: 0},
+          {name: 'Ivan', score: 0},
+          {name: 'Sergey', score: 0},
+          {name: 'Maksim', score: 0},
+          {name: 'Anastasia', score: 0},
+          {name: 'Elena', score: 0},
+          {name: 'Svetlana', score: 0},
+          {name: 'Marina', score: 0}
+            ])
+categories = Category.create!([
+          {title: 'Junior'},
+          {title: 'Middle'},
+          {title: 'Senior'}
+          ])
+
+tests = Test.create!([
+          {title: 'Ruby', level: 1, category_id: Category.all[0].id},
+          {title: 'Ruby', level: 2, category_id: Category.all[1].id},
+          {title: 'Ruby', level: 3, category_id: Category.all[2].id},
+          {title: 'HTML', level: 1, category_id: Category.all[0].id},
+          {title: 'HTML', level: 2, category_id: Category.all[1].id},
+          {title: 'CSS', level: 1, category_id: Category.all[0].id},
+          {title: 'CSS', level: 2, category_id: Category.all[1].id},
+          {title: 'SQL', level: 1, category_id: Category.all[0].id},
+          {title: 'C#', level: 1, category_id: Category.all[0].id},
+          {title: 'C#', level: 2, category_id: Category.all[1].id},
+          {title: 'C#', level: 3, category_id: Category.all[2].id},
+          {title: 'C++', level: 1, category_id: Category.all[0].id},
+          {title: 'C++', level: 3, category_id: Category.all[2].id},
+          {title: 'JS', level: 1, category_id: Category.all[0].id},
+          {title: 'JS', level: 2, category_id: Category.all[1].id},
+          {title: 'Java', level: 2, category_id: Category.all[1].id},
+          {title: 'Java', level: 3, category_id: Category.all[2].id},
+          {title: 'Python', level: 1, category_id: Category.all[0].id},
+          {title: 'Python', level: 2, category_id: Category.all[1].id},
+          {title: 'Go', level: 2, category_id: Category.all[1].id},
+          {title: 'Go', level: 3, category_id: Category.all[2].id}
+                    ])
+                    
+questions = Question.create!([
+          {body: 'Как узнать длину строки в ruby?', test_id: Test.all[0].id},
+          {body: 'Как удалить элемент из массива ruby?', test_id: Test.all[0].id},
+          {body: 'Как преобразовать переменную из числа в строку?', test_id: Test.all[0].id}
+          ])
+
+answers = Answer.create!([
+          {body: 'Длину строки можно узнать с помощью методов size и length', correct: true, question_id: questions[0].id},
+          {body: 'С помощью методов select и chomp', correct: false, question_id: questions[0].id},
+          {body: 'Длину строки можно узнать с помощью метода count', correct: false, question_id: questions[0].id},
+          {body: 'Длину строки можно нельзя узнать в руби!', correct: false, question_id: questions[0].id},
+          {body: 'Удаления элемента из массива выполняется с помощью метода delete', correct: true, question_id: questions[1].id},
+          {body: 'Удаления элекмента выполняется методом remove', correct: false, question_id: questions[1].id},
+          {body: 'Удаления элекмента выполняется методом udalit', correct: false, question_id: questions[1].id},
+          {body: 'С помощью метода to_s', correct: true, question_id: questions[2].id},
+          {body: 'С помощью метода to_c', correct: false, question_id: questions[2].id},
+          {body: 'С помощью метода to_f', correct: false, question_id: questions[2].id}
+          ])
+
+usertests = UserTest.create!([
+          {user_id: users[0].id, test_id: tests[0].id},
+          {user_id: users[1].id, test_id: tests[0].id},
+          {user_id: users[3].id, test_id: tests[0].id},
+          {user_id: users[7].id, test_id: tests[0].id},
+          {user_id: users[11].id, test_id: tests[0].id},
+          {user_id: users[12].id, test_id: tests[0].id},
+          {user_id: users[0].id, test_id: tests[0].id},
+          {user_id: users[0].id, test_id: tests[0].id},
+          {user_id: users[1].id, test_id: tests[0].id}])
+
+#us = User.find_by!(id: 1)
+#tt = Test.find_by!(id: 1)
+#UserTest.create!(user_id: us.id, test_id: tt.id)
+#UserTest.create!(user_id: us.id, test_id: tt.id)
+
+#tt = Test.sort_categories('Middle')
+
+#puts tt
