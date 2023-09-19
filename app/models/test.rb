34fa6113ca388b99032491da0e476e0a547637cc
izulolov <izulolov@mail.ru>
2023-09-20 00:48:02 +0300

    Связи

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions
+  has_and_belongs_to_many :users
+
   def self.sort_categories(category)
     Test.joins('JOIN categories ON categories.id=tests.category_id')
     .where('categories.title =?', category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+  has_and_belongs_to_many :tests
   def tests_by_level(test_level)
     Test.joins('JOIN user_tests ON tests.id = user_tests.test_id')
     .where('user_tests.user_id = ? AND tests.level = ?', id, test_level)
