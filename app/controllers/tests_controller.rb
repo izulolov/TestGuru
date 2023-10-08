2a6b6127967ea1f21283ffd9a95b8ef23c47b9ad
izulolov <izulolov@mail.ru>
2023-10-08 11:59:59 +0300

    The list of test questions is on the same page as the test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,21 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show]
+
+  # GET tests#index
+  # /tests
   def index
     @tests = Test.all
+  end
+
+  # GET tests#show
+  # /tests/:id
+  def show
+
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
   end
 end
