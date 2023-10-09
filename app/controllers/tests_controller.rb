e56970e6dd5dd74ebed2c7af437de3996f76d03a
izulolov <izulolov@mail.ru>
2023-10-09 18:03:26 +0300

    Add create test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -13,7 +13,29 @@ class TestsController < ApplicationController
     
   end
 
+  # GET tests#new
+  # /tests/new
+  def new
+    @test = Test.new
+  end
+
+  # POST tests#create
+  # /tests
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
   private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :user_id)
+  end
 
   def find_test
     @test = Test.find(params[:id])
