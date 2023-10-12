4b201e37a34171bd1e830ea0a8b1e94f62b92889
izulolov <izulolov@mail.ru>
2023-10-12 10:11:39 +0300

    Add helpers 'test questions number'

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -4,18 +4,9 @@ module TestsHelper
 
   def test_level(test)
     TEST_LEVELS[test.level] || :hero
-=begin    case test.level
-    when 0
-      'easy'
-    when 1
-      'elementary'
-    when 2
-      'advanced'
-    when 3
-      'hard'
-    else
-      'hero'
-    end
-=end  
+  end
+
+  def test_questions_number(test)
+    test.questions.count
   end
 end
