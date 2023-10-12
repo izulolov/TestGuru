68b1df4dfd05510c4ab808308a242d415ca4eb2b
izulolov <izulolov@mail.ru>
2023-10-12 14:52:08 +0300

    Add questionheader helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+
+  def question_header(question)
+    question.persisted? ? "Edit #{title(question)} Question" : "Create New #{title(question)} Question"
+  end
+
+  def title(question)
+    Test.find_by(id: question.test_id).title
+  end
 end
