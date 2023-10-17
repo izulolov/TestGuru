92ea3983a953f4851099f90db181884466e72401
izulolov <izulolov@mail.ru>
2023-10-17 14:38:14 +0300

    remove 'def title'

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,8 +3,4 @@ module QuestionsHelper
   def question_header(question)
     question.persisted? ? "Edit #{question.test.title} Question" : "Create New #{question.test.title} Question"
   end
-
-  def title(question)
-    Test.find_by(id: question.test_id).title
-  end
 end
