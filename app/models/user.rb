7e79c1cb9d685e5214a23d51420a2ea383540bb0
izulolov <izulolov@mail.ru>
2024-03-26 10:11:41 +0300

    less10, RubyGems

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
   
-  include Auth
+  #include Auth
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test'
+
+  has_secure_password
 
   def tests_by_level(test_level)
     tests.where(level: test_level)
