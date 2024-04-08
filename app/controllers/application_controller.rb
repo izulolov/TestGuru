abfbd2b7ec8c923ae50ef0fda6acd2696e449e1c
izulolov <izulolov@mail.ru>
2024-04-08 17:21:09 +0300

    Flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery with: :null_session
 
-  helper_method :current_user
-
+  helper_method :current_user,
+                :logged_in?
   private
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path
+      return redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
     end
+
+    cookies[:email] = current_user.email
   end
 
   def current_user
