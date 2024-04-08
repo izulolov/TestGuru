4d9a6b85cc5b7fc649e660ea4aabf8ec32b58692
izulolov <izulolov@mail.ru>
2024-04-08 15:09:10 +0300

    User session

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  #resources :answers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'tests#index'
 
+  #get 'sessions/new'
+  #get 'users/new'
+
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
 
   resources :users, only: :create
+  resources :sessions, only: :create
 
   resources :tests do
     resources :questions, shallow: true do
