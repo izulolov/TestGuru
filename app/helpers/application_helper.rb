d4ae2599683824aa02727c3402db0345729c4b11
izulolov <izulolov@mail.ru>
2024-04-09 13:02:23 +0300

    HomeWork10-2. Create a helper for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,9 @@ module ApplicationHelper
   def github_url(name_repo, url_path)
     link_to name_repo, "https://github.com#{url_path}"
   end
+
+  # 2
+  def flash_message
+    content_tag :p, flash[:alert]
+  end
 end
