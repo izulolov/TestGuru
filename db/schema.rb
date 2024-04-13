9c14f3cd031b8d23b5e14731f1a147a22340ee3b
izulolov <izulolov@mail.ru>
2024-04-13 18:34:45 +0300

    HomeWork10. Added a unique address at the DB level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_25_131321) do
+ActiveRecord::Schema.define(version: 2024_04_13_143813) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 2024_03_25_131321) do
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "demo", primary_key: "ID", force: :cascade do |t|
+    t.string "Name", limit: 20
+    t.text "Hint"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -65,6 +70,7 @@ ActiveRecord::Schema.define(version: 2024_03_25_131321) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
