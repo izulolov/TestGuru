5e7deeec89cc0feb8c67c216b0ac2c49bf6314cc
izulolov <izulolov@mail.ru>
2024-04-16 17:19:03 +0300

    Success migration

--- db/migrate/20240416135853_add_devise_to_users.rb
+++ db/migrate/20240416135853_add_devise_to_users.rb
@@ -40,7 +40,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
     remove_column(:users, :password_digest)
     change_column_default(:users, :email, '')
 
-    add_index :users, :email,                unique: true
+    #add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
