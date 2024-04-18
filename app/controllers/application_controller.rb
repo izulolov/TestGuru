6b02f3e5c0e222679ed7015d6c0f03ba31fb378c
izulolov <izulolov@mail.ru>
2024-04-18 12:34:43 +0300

    HomeWork3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :email, :password, :password_confirmation])
   end
+
+  def after_sign_in_path_for(current_user)
+    current_user.is_a?(Admin) ? admin_tests_path : root_path
+  end
+
 end
