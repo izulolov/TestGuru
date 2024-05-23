a4c070f29c5a7c681c1a2bd49f4a7cbb703a9468
izulolov <izulolov@mail.ru>
2024-05-23 12:28:14 +0300

    save gist on DB

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,20 +1,29 @@
 class GistsController < ApplicationController
   before_action :set_test_passage, only: %i[create]
 
-
+  # Создаем гист в гитхаб если всё ок то добавляем гист еще в бд
   def create
-    result = GistQuestionService.new(@test_passage.current_question)
-    response = result.call
-    flash_options = if result.success?
-      { notice: "#{t('.success')} #{view_context.link_to 'GitHub', response.html_url}" }
+    create_github_gist
+    if @result.success?
+      create_database_gist
+      redirect_to @test_passage, notice: "#{t('.success')} #{view_context.link_to 'GitHub', @response.html_url}"
     else
-      { alert: t('.failure') }
+      redirect_to @test_passage, alert: t('.failure')
     end
-
-    redirect_to @test_passage, flash_options
   end
-  
+
   private
+
+  def create_github_gist
+    @result = GistQuestionService.new(@test_passage.current_question)
+    @response = @result.call
+  end
+
+  def create_database_gist
+    Gist.create(question_id: @test_passage.current_question.id,
+                gist_url: @response.html_url,
+                user_id: @test_passage.user_id)
+  end
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:test_passage_id])
