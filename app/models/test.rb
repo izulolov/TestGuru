90c105c7cb1d0fa5a069c315f4e65ca2647ea7f8
izulolov <izulolov@mail.ru>
2024-05-25 02:40:45 +0300

    main page without empty test

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,7 +8,7 @@ class Test < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  scope :easy1, -> { where(level: (0..1)) }
+  scope :easy, -> { where(level: (0..1)) }
   scope :middle, -> { where(level: (2..4)) }
   scope :difficult, -> { where(level: (5..Float::INFINITY)) }
   scope :sort_by_categories, ->(category) {
