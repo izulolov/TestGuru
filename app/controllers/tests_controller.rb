2e5101b801bb281e56ac1457717870e82bd4f239
izulolov <izulolov@mail.ru>
2024-06-24 23:58:02 +0300

    Can't take the test without questions(empty test)

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -8,8 +8,12 @@ class TestsController < ApplicationController
   end
 
   def start
-    current_user.tests.push(@test)
-    redirect_to current_user.test_passage(@test)
+    if !@test.questions.empty?
+      current_user.tests.push(@test)
+      redirect_to current_user.test_passage(@test)
+    else
+      redirect_to tests_path, alert: t('.failure')
+    end
   end
 
   private
