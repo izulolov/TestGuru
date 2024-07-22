54ec6ca9703fcd1e93f453045229489c770abf2e
izulolov <izulolov@mail.ru>
2024-07-22 11:54:42 +0300

    first admin method

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -1,17 +1,23 @@
 class FeedbacksMailer < ApplicationMailer
-  
+
   def new_feedback_logged_in_user(feedback)
     @last_name = feedback.user.last_name
     @first_name = feedback.user.first_name
     @email = feedback.user.email
     @message = feedback.message
-    mail to: User.where(type: 'Admin').first.email, subject: "Message from #{@last_name} #{@first_name}"
+    mail to: first_admin, subject: "Message from #{@last_name} #{@first_name}"
   end
 
   def new_feedback_for_anonymous_user(feedback)
     @name = feedback[:name]
     @email = feedback[:email]
     @message = feedback[:message]
-    mail to: User.where(type: 'Admin').first.email, subject: "Message from #{@name}"
+    mail to: first_admin, subject: "Message from #{@name}"
+  end
+
+  private
+
+  def first_admin
+    User.where(type: 'Admin').first.email
   end
 end
