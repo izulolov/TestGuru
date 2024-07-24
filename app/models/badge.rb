41a71352ea159a802abe4217ab57627cf16b5e21
izulolov <izulolov@mail.ru>
2024-07-24 21:27:48 +0300

    Validates Badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,6 +1,11 @@
 class Badge < ApplicationRecord
   has_many :user_badges
   has_many :user, through: :user_badges
+
+  validates :name, presence: true
+  validates :image_url, presence: true
+  validates :rule, presence: true
+  
   RULES = { all_tests_backend: 'Все тесты из категории Backend',
           first_attempt: 'Успешное прохождение теста с первой попытки',
           specific_level: 'Успешное прохождение тестов из уровня 1' }.freeze
