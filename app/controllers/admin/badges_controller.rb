83130f9037e64498ace626bfe930d0b345e48ef1
izulolov <izulolov@mail.ru>
2024-07-26 11:12:35 +0300

    Add show, edit, delete, _form to admin

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,9 +1,9 @@
 class Admin::BadgesController < Admin::BaseController
-
+  before_action :set_badge, only: %i[show edit update destroy]
   def index
     @badges = Badge.all
   end
-  
+
   def new
     @badge = Badge.new
   end
@@ -17,9 +17,34 @@ class Admin::BadgesController < Admin::BaseController
     end
   end
 
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to admin_badges_path, notice: "Badge updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path, notice: "Badge deleted successfully."
+  end
+
   private
 
   def badge_params
     params.require(:badge).permit(:name, :image_url, :rule)
+  end
+
+  def set_badge
+    @badge = Badge.find(params[:id])
   end
 end
--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,10 +2,10 @@ class Badge < ApplicationRecord
   has_many :user_badges
   has_many :user, through: :user_badges
 
-  validates :name, presence: true
-  validates :image_url, presence: true
+  validates :name, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :image_url, presence: true, length: { minimum: 10 }
   validates :rule, presence: true
-  
+
   RULES = { all_tests_backend: 'Все тесты из категории Backend',
           first_attempt: 'Успешное прохождение теста с первой попытки',
           specific_level: 'Успешное прохождение тестов из уровня 1' }.freeze
