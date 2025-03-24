3c70889e8575c4931a8406ca50b95ef08a105acb
izulolov <izulolov@mail.ru>
2025-03-24 15:20:47 +0300

    Миграция для добавления поля таймера в таблицу тестов

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_12_203709) do
+ActiveRecord::Schema.define(version: 2025_03_24_121806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2025_01_12_203709) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.boolean "published", default: false, null: false
+    t.integer "timer", default: 0
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
     t.index ["user_id"], name: "index_tests_on_user_id"
