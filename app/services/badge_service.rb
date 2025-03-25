6e49be557e41a2f51fd0c6da9c20b97b8df5bd7f
izulolov <izulolov@mail.ru>
2025-03-25 16:58:15 +0300

    Оптимизация двух метод в сервисе бейдж

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -17,7 +17,7 @@ class BadgeService
 
   def check_badge_rule(badge)
     # Проверяем, что тест пройден успешно
-    return false unless test_passage.successfully_passed?
+    return false unless test_passage.passed
 
     rule_method = "#{badge.rule}?".to_sym
 
@@ -39,38 +39,41 @@ class BadgeService
   end
 
   def passed_all_tests_of_some_level?(badge)
-    # Используем значение уровня из бейджа, а не из текущего теста
+    # Используем значение уровня из бейджа
     level = badge.rule_value.to_i
+    
+    # Проверяю соответсвует уровен теста уровню бейджа
+    return false unless test.level == level
+    
     all_tests_by_level = Test.by_level(level)
-
     return false if all_tests_by_level.empty?
-    return false unless test.level == level # Проверяем, что текущий тест соответствует уровню бейджа
-
-    # Получаем уникальные ID тестов, которые пользователь прошел успешно
+    
     passed_test_ids = user.test_passages.where(passed: true)
                           .where(test_id: all_tests_by_level)
                           .select(:test_id).distinct.pluck(:test_id)
-
-    # Проверяем, что пользователь прошел все тесты этого уровня
+    
     all_tests_by_level.count == passed_test_ids.count
   end
 
   def passed_all_tests_of_some_category?(badge)
     # Используем значение категории из бейджа
     category_id = badge.rule_value.to_i
+    
+    # Сначала проверяем, соответствует ли текущий тест категории бейджа
+    return false unless test.category_id == category_id
+    
     category = Category.find_by(id: category_id)
-
     return false unless category
+    
+    # Теперь делаем запрос к БД только если предыдущая проверка прошла успешно
     all_tests_by_category = Test.by_category(category)
-
     return false if all_tests_by_category.empty?
-    return false unless test.category_id == category_id # Проверяем, что текущий тест соответствует категории бейджа
-
+    
     # Получаем уникальные ID тестов, которые пользователь прошел успешно
     passed_test_ids = user.test_passages.where(passed: true)
                           .where(test_id: all_tests_by_category)
                           .select(:test_id).distinct.pluck(:test_id)
-
+    
     # Проверяем, что пользователь прошел все тесты этой категории
     all_tests_by_category.count == passed_test_ids.count
   end
